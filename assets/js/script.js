b5f0c15c98c59bcf30779af0b5cce2b7ea413e3c
wramadhan482 <53812524+wramadhan482@users.noreply.github.com>
2022-07-08 19:19:06 +0700

    cek11

--- assets/js/script.js
+++ assets/js/script.js
@@ -193,7 +193,7 @@ var Tawk_API = Tawk_API || {},
   var s1 = document.createElement("script"),
     s0 = document.getElementsByTagName("script")[0];
   s1.async = true;
-  s1.src = "https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6";
+  s1.src = "https://embed.tawk.to/62c81795b0d10b6f3e7b6ccd/1g7epo8v0";
   s1.charset = "UTF-8";
   s1.setAttribute("crossorigin", "*");
   s0.parentNode.insertBefore(s1, s0);
