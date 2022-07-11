b576ef01ed583f75c2818d794b5a0a2c98a87f88
wramadhan482 <53812524+wramadhan482@users.noreply.github.com>
2022-07-11 12:53:33 +0700

    change icon title visibility

--- projects/script.js
+++ projects/script.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
 document.addEventListener("visibilitychange", function () {
   if (document.visibilityState === "visible") {
     document.title = "Projects | Portfolio Wahyu Ramadhan";
-    $("#favicon").attr("href", "/assets/images/favicon.png");
+    $("#favicon").attr("href", "/assets/images/AvatarMaker.svg");
   } else {
     document.title = "Come Back To Portfolio";
     $("#favicon").attr("href", "/assets/images/favhand.png");
